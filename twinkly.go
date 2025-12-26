a48831e713402a14b464e0e6834d33fefd33f820
Roman Mazur <roman.mazur@gmail.com>
2025-12-27 00:10:58 +0200

    ++ New methods
    GetInfo
    GetMode
    GetColor
    GetBrightness

--- twinkly.go
+++ twinkly.go
@@ -12,6 +12,35 @@ import (
 	"time"
 )
 
+type Gestalt struct {
+	FWFamily          string  `json:"fw_family"`           // F/G only
+	ProductName       string  `json:"product_name"`        // D/F/G
+	ProductVersion    string  `json:"product_version"`     // D only (numeric string)
+	HardwareVersion   string  `json:"hardware_version"`    // D/F/G (numeric string)
+	BytesPerLed       int     `json:"bytes_per_led"`       // D/F
+	FlashSize         int     `json:"flash_size"`          // D/F/G
+	LedType           int     `json:"led_type"`            // D/F/G
+	LedVersion        string  `json:"led_version"`         // D only
+	ProductCode       string  `json:"product_code"`        // D/F/G
+	DeviceName        string  `json:"device_name"`         // D/F/G
+	RSSI              int     `json:"rssi"`                // D only (>=2.1.0)
+	Uptime            string  `json:"uptime"`              // D: seconds string, F/G: milliseconds string
+	HWID              string  `json:"hw_id"`               // D/F/G
+	MAC               string  `json:"mac"`                 // D/F/G
+	UUID              string  `json:"uuid"`                // D/F/G
+	MaxSupportedLed   int     `json:"max_supported_led"`   // D/F/G
+	BaseLedsNumber    int     `json:"base_leds_number"`    // D only
+	NumberOfLed       int     `json:"number_of_led"`       // D/F/G
+	LedProfile        string  `json:"led_profile"`         // D/F/G
+	FrameRate         float64 `json:"frame_rate"`          // D:int but ok as float64, F/G: float
+	MeasuredFrameRate float64 `json:"measured_frame_rate"` // F/G only (>=2.5.6)
+	MovieCapacity     int     `json:"movie_capacity"`      // D/F/G
+	Copyright         string  `json:"copyright"`           // D/F/G
+	WireType          int     `json:"wire_type"`           // G only
+
+	Code int `json:"code"` // app return code
+}
+
 type LoginResponse struct {
 	AuthenticationToken string `json:"authentication_token"`
 	ChallengeResponse   string `json:"challenge-response"`
@@ -22,6 +51,14 @@ type Color struct {
 	Red   int `json:"red"`
 	Green int `json:"green"`
 	Blue  int `json:"blue"`
+}
+
+type Brightness struct {
+	Value int `json:"value"`
+}
+
+type Mode struct {
+	Mode string `json:"mode"`
 }
 
 type Movie struct {
@@ -106,6 +143,43 @@ func (tm *TwinklyManager) startTokenRefresh() {
 	}()
 }
 
+func (tm *TwinklyManager) GetInfo() (Gestalt, error) {
+
+	token, err := tm.GetToken()
+	if err != nil {
+		return Gestalt{}, err
+	}
+	return tm.TWgetGestalt(token)
+}
+
+func (tm *TwinklyManager) GetColor() (Color, error) {
+
+	token, err := tm.GetToken()
+	if err != nil {
+		return Color{}, err
+	}
+	return tm.TWgetColor(token)
+}
+
+func (tm *TwinklyManager) GetBrightness() (int, error) {
+	token, err := tm.GetToken()
+	if err != nil {
+		return 0, err
+	}
+	return tm.TWgetBrightness(token)
+}
+
+func (tm *TwinklyManager) GetMode() (string, error) {
+	token, err := tm.GetToken()
+	if err != nil {
+		return "", err
+	}
+
+	mode, err := tm.TWgetMode(token)
+
+	return mode, err
+}
+
 func (tm *TwinklyManager) SetColor(color Color) error {
 
 	token, err := tm.GetToken()
@@ -163,6 +237,20 @@ func (tm *TwinklyManager) TWverify(challengeResponse, token string) error {
 	return err
 }
 
+func (tm *TwinklyManager) TWgetGestalt(token string) (Gestalt, error) {
+	body, err := tm.TWrequest("GET", "/xled/v1/gestalt", "", token)
+	if err != nil {
+		return Gestalt{}, err
+	}
+
+	var g Gestalt
+	if err := json.Unmarshal(body, &g); err != nil {
+		return Gestalt{}, err
+	}
+
+	return g, nil
+}
+
 func (tm *TwinklyManager) TWsetColor(color Color, token string) error {
 	mode := map[string]string{"mode": "color"}
 	_, err := tm.TWrequest("POST", "/xled/v1/led/mode", mode, token)
@@ -170,10 +258,55 @@ func (tm *TwinklyManager) TWsetColor(color Color, token string) error {
 	return err
 }
 
+func (tm *TwinklyManager) TWgetColor(token string) (Color, error) {
+
+	body, _ := tm.TWrequest("GET", "/xled/v1/led/color", "", token)
+
+	var resp Color
+
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return Color{}, err
+	}
+
+	return Color{
+		Red:   resp.Red,
+		Green: resp.Green,
+		Blue:  resp.Blue,
+	}, nil
+}
+
 func (tm *TwinklyManager) TWsetBrightness(brightness int, token string) error {
 	data := map[string]interface{}{"mode": "enabled", "type": "A", "value": brightness}
 	_, err := tm.TWrequest("POST", "/xled/v1/led/out/brightness", data, token)
 	return err
+}
+
+func (tm *TwinklyManager) TWgetBrightness(token string) (int, error) {
+	body, err := tm.TWrequest("GET", "/xled/v1/led/out/brightness", "", token)
+	if err != nil {
+		return 0, err
+	}
+
+	var resp Brightness
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return 0, err
+	}
+
+	return resp.Value, nil
+}
+
+func (tm *TwinklyManager) TWgetMode(token string) (string, error) {
+	body, err := tm.TWrequest("GET", "/xled/v1/led/mode", "", token)
+	if err != nil {
+		return "", err
+	}
+
+	var resp Mode
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return "", err
+	}
+
+	return resp.Mode, nil
 }
 
 func (tm *TwinklyManager) TWsetMovie(id int, token string) error {
